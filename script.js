033d9f79837569d05b0bca6b17e9ea6ad4794d34
Dmitrii Fufachev <Dima@Dmitriis-MacBook-Pro.local>
2018-10-30 01:39:47 +0000

    Homework 7

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ const GAMES_TO_PLAY = 10;
  * Eftir leik er notanda boðið að spila annan leik, ef ekki hættir forrit.
  */
 function start() {
-  villa;
+  alert(`Markmið er að svara eins mörgum af ${GAMES_TO_PLAY} dæmum rétt eins hratt og mögulegt er.`)
+  do {
+    play();
+  } while (confirm('Spila annan leik?'));
 }
 
 /**
@@ -28,28 +31,88 @@ function start() {
  *
  */
 function play() {
+  let time = Date.now();
+  var cancel = false;
+  var correct = 0;
+  for (var i = 0; i < GAMES_TO_PLAY; i++) {
+    var [corr, ans] = ask();
+
+    if (ans === null) {
+      cancel = true;
+      alert("Hætt í leik.")
+      break;
+    }
+
+    if (corr) {
+      correct++;
+    }
+  }
+
+  if (!cancel) {
+    time = (Date.now() - time) / 1000;
+    var Y = time.toFixed(2);
+    var Z = (correct / time).toFixed(2);
+
+    alert(`Þú svaraðir ${correct} af ${GAMES_TO_PLAY} dæmum rétt á ${Y} sekúndum\n` +
+      `Meðalrétt svör á sekúndu eru ${Z}`);
+  }
 }
 
-/**
- * Spyr einnar spurningar og skilar upplýsingum um svar (mögulega með því að
- * nota true, false og null ef notandi hættir). Birtir notanda propmpt til að
- * svara í og túlkar svarið yfir í tölu.
- *
- * Mögulegar spurningar eru:
- * - `+` dæmi þar sem báðar tölur geta verið á bilinu `[1, 100]`
- * - `-` dæmi þar sem báðar tölur geta verið á bilinu `[1, 100]`
- * - `*` dæmi þar sem báðar tölur geta verið á bilinu `[1, 10]`
- * - `/` dæmi þar sem fyrri tala er á bilinu `[2, 10]` og seinni talan er fyrri
- *   talan sinnum tala á bilinu `[2, 10]` þ.a. svarið verði alltaf heiltala
- *
- * Sniðugt væri að færa það að búa til spurningu í nýtt fall sem ask() kallar í.
- */
+// List of all operations
+function question() {
+  switch (randomNumber(1, 4)) {
+    case 1: // Addition
+      var min = 1;
+      var max = 100;
+      var numtwo = randomNumber(min, max);
+      var numone = randomNumber(min, max);
+      var sol = numone + numtwo;
+      return [numone, '+', numtwo, sol];
+    case 2: // Subtraction
+      var min = 1;
+      var max = 100;
+      var numtwo = randomNumber(min, max);
+      var numone = randomNumber(min, max);
+      var temp1 = Math.max(numone, numtwo);
+      var temp2 = Math.min(numone, numtwo);
+      var sol = temp1 - temp2;
+      return [temp1, '-', temp2, sol];
+    case 3: // Multiplication    
+      var min = 1;
+      var max = 10;
+      var oper = "*";
+      var numtwo = randomNumber(min, max);
+      var numone = randomNumber(min, max);
+      var sol = numone * numtwo;
+      return [numone, '*', numtwo, sol];
+    case 4: // Division
+      var min = 2;
+      var max = 10;
+      var multiple = randomNumber(min, max);
+      var numtwo = randomNumber(min, max);
+      var numone = multiple * numtwo;
+      var sol = numone / numtwo;
+      return [numone, '/', numtwo, sol];
+  }
+}
+
 function ask() {
+  let [numone, oper, numtwo, sol] = question();
+  const input = prompt(`Hvað er ${numone}${oper}${numtwo}?`);
+  const parsedInput = parseGuess(input);
+  if (parsedInput === sol) {
+    return [true, parsedInput];
+  }
+  return [false, parsedInput];
 }
 
-/**
- * Skilar tölu af handahófi á bilinu [min, max]
- */
+function parseGuess(input) {
+  const parsed = parseInt(input, 10);
+  if (isNaN(parsed)) return null;
+  return parsed;
+}
+
+/* Skilar tölu af handahófi á bilinu [min, max] */
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
